4e4951fd039c0a3388793051489a72b7172fb18e
Aryan152005 <paratakke@gmail.com>
2025-03-17 22:45:09 +0530

    Reverted

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Supabase credentials from your project
-const supabaseUrl =  import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://ontbsbhtyoruxofazyrx.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9udGJzYmh0eW9ydXhvZmF6eXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3MjA2NTMsImV4cCI6MjA1NjI5NjY1M30.vSHr3jLOOg7Sd3C6F5gfxVPXMd5Orr5YM0k2jBO5WEg'
+
 // Create a single supabase client for the entire app
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
