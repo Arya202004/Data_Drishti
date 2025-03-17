16d139cbbb394f9d97eed81ea5c20cccb8cadf17
Aryan Paratakke <127593522+Aryan152005@users.noreply.github.com>
2025-03-17 22:19:20 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Supabase credentials from your project
-const supabaseUrl =  import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =  import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 // Create a single supabase client for the entire app
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
